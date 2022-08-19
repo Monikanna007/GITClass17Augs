c37be6c70e6c8a69d9a80f19f07b9a9e8915fa5a
Monikanna007 <kannamonishak@gmail.com>
2022-08-19 03:11:29 -0700

    moni

---
+++ src/org/test/Hello.java
@@ -0,0 +1,31 @@
+package org.test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+	
+	//datetype varb = value;
+	   
+	int a = 10;
+	
+	//camparing or checking
+	if (a<= 20) {
+		System.out.println(a);
+		
+	}
+	System.out.println("done");
+
+	}	
+	
+}
+	
+
+
+
+	
+
+
+
+
+
